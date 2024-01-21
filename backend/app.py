fe773bf72b7e0547023eb9d5109323bb45e6a3b0
surface-integral <wang5489@purdue.edu>
2024-01-20 22:00:09 -0500

    Added some css stuff lol

--- backend/app.py
+++ backend/app.py
@@ -1,34 +1,42 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect
 import os
 
 app = Flask(__name__)
 
-# Set the upload folder
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 UPLOAD_FOLDER = 'backend/saved_images'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/upload', methods=['POST'])
+@app.route('/upload_image', methods=['POST'])
 def upload_file():
     # Check if the POST request has a file part
     if 'file' not in request.files:
-        return redirect(request.url)
+        return render_template('invalid_file.html')
     
     file = request.files['file']
+
+    if not allowed_file(file.filename):
+        return render_template('invalid_file.html')
     
-    # If the user does not select a file, the browser sends an empty file
+    # If the user does not select a file, return page indicating an error
     if file.filename == '':
-        return redirect(request.url)
+        return render_template('no_file.html')
     
     # Save the uploaded file to the uploads folder
     if file:
         filename = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
         file.save(filename)
-        print(filename)
-        return render_template('upload_success.html', filename=filename)
+        print(file.filename)
+        return render_template('index.html', file=filename)
 
 if __name__ == '__main__':
     # Ensure the 'uploads' folder exists
